29d7201dcc0f33b92b0ea6136fc5464bfbceae61
osprey74 <so4330@osprey74.com>
2026-04-01 11:09:44 +0900

    s

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/osprey74/michinavi/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.osprey74.michinavi.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.osprey74.michinavi.test";
+  public static final String BUILD_TYPE = "debug";
+}
